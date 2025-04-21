71a754208b1cc68e876e596ed4394d90167e053a
SourabhRJoshi <sourabhrjoshi@gmail.com>
2025-04-21 12:46:05 +0530

    third commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main{
         System.out.println("This is a test.");
         System.out.println(new Date());
         System.out.println("End of program.");
+        System.out.println("Program completed successfully.");
     }
 }
