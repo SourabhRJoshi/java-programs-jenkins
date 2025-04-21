a5b08af4a4e749e7db0c24a5c6b8cc28fd023090
SourabhRJoshi <sourabhrjoshi@gmail.com>
2025-04-21 14:06:34 +0530

    added new file

--- Main.java
+++ Main.java
@@ -9,5 +9,8 @@ public class Main{
         System.out.println("Program completed successfully.");
         System.out.println("Added one more line for testing.");
         System.out.println("This is a new line added for testing purposes.");
+
+        Student std = new Student();
+        std.displayInfo();
     }
 }
---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    
+    Student(){
+        System.out.println("Student object created.");
+    }
+
+    public void displayInfo() {
+        System.out.println("Displaying student information.");
+    }
+    
+}
